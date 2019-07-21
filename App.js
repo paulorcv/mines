8bc4240ed61a5d64f7053ecffc25f869d990751a
Paulo Campos <paulorcv@gmail.com>
2019-07-21 20:05:36 -0300

    Field component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import params from './src/params';
+import Field from './src/components/Field';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
+        <Field />
       </View>
     );
   }
