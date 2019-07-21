67ca331c568dbd08b7b1000121f6ffb0aa228307
Paulo Campos <paulorcv@gmail.com>
2019-07-21 20:22:56 -0300

    Field styles

--- App.js
+++ App.js
@@ -13,6 +13,11 @@ class App extends Component {
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
         <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
       </View>
     );
   }
